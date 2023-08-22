1bb4d1bba54bdbc427c14f5e1eb4c9f685cdc16b
xsr <ogienognisty@gmail.com>
2023-08-22 13:20:05 +0300

    naive color theme switcher

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import {createRoot} from "react-dom/client"
 // import styled from "styled-components"
 // import Index from "./index.js"
+import { ColorMode } from "./components"
 
 const
   syntax = __ => __.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, match => "<span class='" + (/^"/.test(match) ? (/:$/.test(match) ? "key" : "string") : (/true|false/.test(match) ? "boolean" : (/null/.test(match) ? "null" : "number"))) + "'>" + match + "</span>"),
@@ -10,7 +11,8 @@ const
   // queryClient = new QueryClient()
   Index = () => <main>
     <h3>React + Tailwind + Webpack</h3>
-    <h1>Landing PWA</h1>
+    <h1>Landing App</h1>
+    <ColorMode /> 
     <Debug data={{}} />
   </main>
 
